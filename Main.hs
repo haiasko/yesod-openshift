c4cd83172a77a2ae435557e957e3d6efb23f65d1
Plamen Nikolaev <plamen@gmail.com>
2014-05-18 22:47:21 +0200

    testing, first steps, learning to run warp with a hostname and port

--- Main.hs
+++ Main.hs
@@ -1,27 +1,23 @@
-import Control.Monad
-import Control.Exception
-import Data.Char
-import System.IO
-import System.IO.Error
-import Network
-import Network.BSD
-import Network.Socket
-import System.Environment
-import System.Process
+{-# LANGUAGE QuasiQuotes           #-}
+{-# LANGUAGE TemplateHaskell       #-}
+{-# LANGUAGE TypeFamilies          #-}
+import           Yesod
 
-main = do sock <- bracketOnError
-                     (getProtocolNumber "tcp" >>= socket AF_INET Stream)
-                     close
-                     (\sock -> do setSocketOption sock ReuseAddr 1
-                                  [host,port] <- getArgs
-                                  inet <- inet_addr host
-                                  bindSocket sock $ SockAddrInet (fromIntegral $ read port) inet
-                                  listen sock maxListenQueue
-                                  return sock
-                     )
-          packages <- readProcess "ghc-pkg" ["list", "--simple-output"] []
-          let response = "HTTP/1.1 200 OK\r\n\r\nWelcome to Hatraiaskell Cloud! The following packages are pre-installed:\n\n" ++ unlines (words packages)
-          forever $ do (handle,_,_) <- Network.accept sock
-                       read <- liftM (any (null . dropWhile isSpace) . lines) $ hGetContents handle
-                       when read $ void $ tryIOError $ hPutStr handle response
-                       hClose handle
+data HelloWorld = HelloWorld
+
+mkYesod "HelloWorld" [parseRoutes|
+/ HomeR GET
+|]
+
+instance Yesod HelloWorld
+
+getHomeR :: Handler Html
+getHomeR = defaultLayout [whamlet|Hello World!|]
+
+main :: IO ()
+main = do 
+    [host,port] <- getArgs
+    putStrLn $ "Listening on host " ++ host ++ " port " ++ show port
+    let settings = defaultSettings { settingsPort = port,
+                                     settingsHost = host }
+    runSettings settings HelloWorld
