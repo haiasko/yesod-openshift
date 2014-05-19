149c65d3684ad32e3559f151af59cc06cf2b118b
Plamen Nikolaev <plamen@gmail.com>
2014-05-19 03:07:08 +0200

    added

---
+++ MyWarp.hs
@@ -0,0 +1,17 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module MyWarp (
+  myWarp
+) where
+
+import System.Environment
+
+import Network.Wai.Handler.Warp
+
+import Yesod.Dispatch
+
+myWarp app = do
+    [host,port] <- getArgs
+    putStrLn $ "Listening on host " ++ host ++ " port " ++ show port
+    let settings = setPort (read port) $ setHost host defaultSettings
+    runSettings settings $ toWaiApp app
