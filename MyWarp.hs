d4462d272b613b0c38afc2560a04a3274be1b4ab
Plamen Nikolaev <plamen@gmail.com>
2014-05-19 05:05:52 +0200

    .

--- MyWarp.hs
+++ MyWarp.hs
@@ -13,5 +13,9 @@ import Yesod
 myWarp app = do
     [host,port] <- getArgs
     putStrLn $ "Listening on host " ++ host ++ " port " ++ show port
-    let settings = setPort (read port) $ setHost host defaultSettings
+
+-- This requires warp 2.1.0 and we have 2.0.4
+--    let settings = setPort (read port) $ setHost host defaultSettings
+    let settings = defaultSettings { settingsPort = (read port),
+                                     settingsHost = Host host }
     runSettings settings $ toWaiApp app
